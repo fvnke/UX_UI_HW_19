f4dc44ffd3d5ebd45a478a032199b01a568069cb
fvnke <74390105+fvnke@users.noreply.github.com>
2020-12-20 18:26:29 -0500

    Dec 20 part 3

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Your index.js file is loaded correctly");
+
+
+
+//$(document).ready(function(){;
+
+$(function(){
+
+    $(".content").ready(function(){;
+
+      $(".content").fadeIn(slow);
+    });
+  });
+
+
+//$(".content").fadeIn(speed,callback);
+
+
+//$(document).ready(function(){
+
+   // $(".content").ready()
+   // $(".content").fadeIn(1000);
+//})
+
